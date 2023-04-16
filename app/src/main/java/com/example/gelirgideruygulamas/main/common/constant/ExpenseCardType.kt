f2dd82315780bdecdf5f29d98513cfb953f29bfa
Emin <94618987+emindemir1541@users.noreply.github.com>
2023-04-16 14:53:47 +0300

    Kotlin and android studio update

--- app/src/main/java/com/example/gelirgideruygulamas/main/common/constant/ExpenseCardType.kt
+++ app/src/main/java/com/example/gelirgideruygulamas/main/common/constant/ExpenseCardType.kt
@@ -1,7 +1,6 @@
 package com.example.gelirgideruygulamas.main.common.constant
 
 object ExpenseCardType {
-    const val DATE_CARD = 0
     const val EXPENSE_UNDONE_CARD = 1
     const val LINE_CARD = 2
     const val EXPENSE_DONE_CARD = 3
