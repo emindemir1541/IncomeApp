6d5fc762b15d9d2feac7b041de5c30dbb14e36a8
Emin <94618987+emindemir1541@users.noreply.github.com>
2023-04-18 13:23:42 +0300

    checkpoint: splashScreen

--- app/src/main/java/com/example/gelirgideruygulamas/main/MainActivity.kt
+++ app/src/main/java/com/example/gelirgideruygulamas/main/MainActivity.kt
@@ -1,28 +1,42 @@
 package com.example.gelirgideruygulamas.main
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.gelirgideruygulamas.R
 import com.example.gelirgideruygulamas.main.data.sharedPreference.PageSettings
 import com.example.gelirgideruygulamas.databinding.ActivityMainBinding
 import com.example.gelirgideruygulamas.expense.ui.fragment.FragmentExpense
 import com.example.gelirgideruygulamas.income.ui.fragment.FragmentIncome
+import com.example.gelirgideruygulamas.main.data.viewmodel.MainViewModel
 import com.example.gelirgideruygulamas.main.ui.fragment.FragmentMain
 
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val mainViewModel: MainViewModel by viewModels()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen().apply {
+            setKeepOnScreenCondition {
+                mainViewModel.isLoading.value
+            }
+        }
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         setNavigationBar()
         binding.bottomNavigation.selectedItemId = R.id.itemHome
         setFragmentMain()
+        mainViewModel._isLoading.value = false
+
+
+
 
 
     }
