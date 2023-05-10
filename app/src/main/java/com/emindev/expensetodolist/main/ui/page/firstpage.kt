372f3b0d64d81a5572541476ae26028cd30b1b21
emin <emindemir1543@gmail.com>
2023-05-10 19:38:42 +0300

    compose implemented

--- app/src/main/java/com/emindev/expensetodolist/main/ui/page/firstpage.kt
+++ app/src/main/java/com/emindev/expensetodolist/main/ui/page/firstpage.kt
@@ -1,14 +1,14 @@
-package com.emindev.expensetodolist.main.ui.page/*
 package com.emindev.expensetodolist.main.ui.page
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+
 
 @Composable
-fun StartPage(){
-Box(modifier = Modifier.fillMaxSize().background(Color.Blue))
-}*/
+fun FirstPage(){
+    Box(modifier = Modifier.fillMaxSize()){
+
+    }
+}
